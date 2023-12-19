3995a46da667b5040f4797d1b9b067c3f7c83919
Joseph Ogelenya <josephoges9@gmail.com>
2023-12-19 14:09:25 +0100

    Add files via upload

---
+++ app/main.js
@@ -0,0 +1,106 @@
+let menuToggle = document.querySelector(".navbar__menuon img")
+let navbar__col2 = document.querySelector(".navbar__col2")
+let navbar__col3 = document.querySelector(".navbar__col3")
+let navbar = document.querySelector(".navbar")
+
+const carousel = document.querySelector('.carousel')
+let review = document.getElementsByClassName("review")
+let next = document.querySelector(".next")
+let previous = document.querySelector(".prev")
+let place = 0;
+let touchStartX;
+
+let toggleRow = document.getElementsByClassName("toggle-row")
+let toggleText = document.getElementsByClassName("toggle-text")
+let toggleArrow = document.getElementsByClassName("toggle-arrow")
+
+menuToggle.addEventListener("click", function () {
+    if (navbar__col2.style.maxHeight === "0" || navbar__col2.style.maxHeight === "") {
+        menuToggle.src = "images/import.png"
+        navbar__col2.style.maxHeight = "500px"
+        navbar__col2.style.transition = "max-height 0.6s ease-in"
+        navbar__col2.style.marginBlock = "1rem"
+        navbar__col3.style.maxHeight = "500px"
+        navbar__col3.style.transition = "max-height 1.2s ease-in"
+        navbar.style.height = "auto"
+    } else {
+        menuToggle.src = "images/bars-3.svg"
+        navbar__col2.style.maxHeight = ""
+        navbar__col2.style.transition = ""
+        navbar__col2.style.marginBlock = ""
+        navbar__col3.style.maxHeight = ""
+        navbar__col3.style.transition = ""
+        navbar.style.height = ""
+    }
+})
+
+const nextSlide = () => {
+    place = (place + 1) % review.length
+    //console.log(place)
+
+    for (let i = 0; i < review.length; i++) {
+        review[0].style.order = (place + 1) % review.length
+        review[1].style.order = (place + 2) % review.length
+        review[2].style.order = (place + 3) % review.length
+    }
+}
+
+const previousSlide = () => {
+    place = (place - 1) % 3;
+    for (let i = 0; i < review.length; i++) {
+        review[0].style.order = (place - 1) % review.length
+        review[1].style.order = (place - 2) % review.length
+        review[2].style.order = (place - 3) % review.length
+    }
+}
+
+next.addEventListener("click", () => {
+    nextSlide()
+})
+
+previous.addEventListener("click", () => {
+    previousSlide()
+})
+
+
+carousel.addEventListener('touchstart', (e) => {
+    touchStartX = e.touches[0].clientX;
+});
+
+carousel.addEventListener('touchend', (e) => {
+    const touchEndX = e.changedTouches[0].clientX;
+    const deltaX = touchEndX - touchStartX;
+
+    if (deltaX > 50) {
+        previousSlide()
+    } else if (deltaX < -50) {
+        nextSlide()
+    }
+});
+
+const runAccordion = (index) => {
+    if (toggleText[index].style.display === "none" || toggleText[index].style.display === "") {
+        toggleText[index].style.display = "block"
+        toggleArrow[index].style.transform = "rotate(180deg)"
+        toggleArrow[index].style.transition = "transform 150ms ease-out"
+    } else {
+        toggleText[index].style.display = "none"
+        toggleArrow[index].style.transform = "none"
+    }
+}
+
+toggleRow[0].addEventListener("click", () => {
+    runAccordion(0)
+})
+
+toggleRow[1].addEventListener("click", () => {
+    runAccordion(1)
+})
+
+toggleRow[2].addEventListener("click", () => {
+    runAccordion(2)
+})
+
+toggleRow[3].addEventListener("click", () => {
+    runAccordion(3)
+})
---
+++ app/notes.md
@@ -0,0 +1,35 @@
+@include p.break(zero, mobile) {
+
+}
+
+@include p.break(desktop, infinity) {
+    
+}
+
+@include p.break(tabletmin, tabletmax) {
+    
+}
+
+clamp(rem, vw, )
+
+loop throught the reviews class
+
+on next click, each of their flex orders increments by one % collection.lenght
+on prev click, each of their flex orders decrements by one % collection.lenght
+
+toDo
+
+work on reducing sans serif default font weight --done
+work on full responsivity --done
+    clamp rest of top --done
+    tablet responsivity --done
+work on user interactivity --done
+    hover states --done
+work on accessibility
+    keyboard --done
+    screen reader --done
+        semantic html --done
+        alt attributes for images --done
+        select label --done
+work on transitions --done
+work on changing page language through select
